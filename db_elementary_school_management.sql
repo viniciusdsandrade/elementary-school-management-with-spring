27ea4d7f985b348b1c94cad107e984fa99c9e154
Andrade <vinicius_andrade2010@hotmail.com>
2024-02-25 12:49:25 -0300

    feat: db_elementary_school_management

--- db_elementary_school_management.sql
+++ db_elementary_school_management.sql
@@ -4,24 +4,66 @@ USE db_elementary_school_management;
 
 SELECT DATABASE() AS 'Current Database';
 
-SELECT * FROM tb_teacher;
+# Enums
 SELECT * FROM tb_discipline;
+SELECT * FROM tb_period;
+SELECT * FROM tb_room;
+SELECT * FROM tb_time;
 
-CREATE TABLE IF NOT EXISTS tb_teacher
+SELECT * FROM tb_student;
+SELECT * FROM tb_teacher;
+
+CREATE TABLE IF NOT EXISTS tb_student
 (
-    id         BIGINT UNSIGNED AUTO_INCREMENT,
-    name       VARCHAR(50) NOT NULL,
-    cpf        VARCHAR(50) NOT NULL,
-    email      VARCHAR(50) NOT NULL,
-    phone      VARCHAR(50) NOT NULL,
-    address    VARCHAR(50) NOT NULL,
-    birth_date DATE        NOT NULL,
+    id                 BIGINT UNSIGNED AUTO_INCREMENT,
+    name               VARCHAR(50) NOT NULL,
+    email              VARCHAR(50) NOT NULL,
+    cpf                VARCHAR(50) NOT NULL,
+    birth_date         DATE        NOT NULL,
+    parent_name        VARCHAR(50) NOT NULL,
+    parent_email       VARCHAR(50) NOT NULL,
+    parent_phone       VARCHAR(50) NOT NULL,
+    allergies          VARCHAR(50) NULL,
+    medical_conditions VARCHAR(50) NULL,
+    street             VARCHAR(50) NOT NULL,
+    number             VARCHAR(50) NOT NULL,
+    complement         VARCHAR(50) NULL,
+    neighborhood       VARCHAR(50) NOT NULL,
+    city               VARCHAR(50) NOT NULL,
+    state              VARCHAR(50) NOT NULL,
+    country            VARCHAR(50) NOT NULL,
+    zip_code           VARCHAR(50) NOT NULL,
 
     UNIQUE (email),
     UNIQUE (cpf),
 
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS tb_teacher
+(
+    id           BIGINT UNSIGNED AUTO_INCREMENT,
+    name         VARCHAR(50) NOT NULL,
+    email        VARCHAR(50) NOT NULL,
+    cpf          VARCHAR(50) NOT NULL,
+    birth_date   DATE        NOT NULL,
+    street       VARCHAR(50) NOT NULL,
+    number       VARCHAR(50) NOT NULL,
+    complement   VARCHAR(50) NULL,
+    neighborhood VARCHAR(50) NOT NULL,
+    city         VARCHAR(50) NOT NULL,
+    state        VARCHAR(50) NOT NULL,
+    country      VARCHAR(50) NOT NULL,
+    zip_code     VARCHAR(50) NOT NULL,
+
+    UNIQUE (email),
+    UNIQUE (cpf),
+
+    PRIMARY KEY (id)
+);
+
+
+
 
 CREATE TABLE IF NOT EXISTS tb_discipline
 (
@@ -48,7 +90,6 @@ CREATE TABLE IF NOT EXISTS tb_discipline
     PRIMARY KEY (id)
 );
 
-
 CREATE TABLE IF NOT EXISTS tb_period
 (
     id   BIGINT UNSIGNED AUTO_INCREMENT,
@@ -74,6 +115,21 @@ CREATE TABLE IF NOT EXISTS tb_room
         'SALA09',
         'SALA10'
         ) NOT NULL,
+
+    UNIQUE (name),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS tb_time
+(
+    id   BIGINT UNSIGNED AUTO_INCREMENT,
+    name ENUM (
+        'MORNING',
+        'AFTERNOON',
+        'EVENING',
+        'NIGHT',
+        'INTEGRAL') NOT NULL,
 
     UNIQUE (name),
 
